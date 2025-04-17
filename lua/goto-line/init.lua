6aa87366948eecdf3e62956dc9d784c42ba6d1f1
Devansh Sharma <devansh.sharma@gmail.com>
2025-04-18 01:03:56 +0530

    fix: Break on first valid file match

--- lua/goto-line/init.lua
+++ lua/goto-line/init.lua
@@ -15,28 +15,32 @@ function M.GotoLine()
   ---@type string[]
   local matcher_prefix = { "/", "%./", "%.%./", "~", "" }
 
+  ---@type boolean
+  local matched = false
   for _, prefix in ipairs(matcher_prefix) do
-    for _, pattern in ipairs({ file_path_matcher, file_path_matcher_no_spaces }) do
-      ---@type integer|nil, integer|nil
-      local start, finish = line:find(prefix .. pattern)
+    if not matched then
+      for _, pattern in ipairs({ file_path_matcher, file_path_matcher_no_spaces }) do
+        ---@type integer|nil, integer|nil
+        local start, finish = line:find(prefix .. pattern)
 
-      if start ~= nil and col >= start and col <= finish then
-        ---@type string
-        local str = line:sub(start, finish)
-        ---@type string, string
-        local filename, line_number = str:match("(.*):(%d+)")
-        if filename ~= nil and line_number ~= nil then
-          -- Expand `~` as uv.fs_stat does not expand it
-          filename = vim.fn.expand(filename)
+        if start ~= nil and col >= start and col <= finish then
+          ---@type string
+          local str = line:sub(start, finish)
+          ---@type string, string
+          local filename, line_number = str:match("(.*):(%d+)")
+          if filename ~= nil and line_number ~= nil then
+            -- Expand `~` as uv.fs_stat does not expand it
+            filename = vim.fn.expand(filename)
 
-          ---@type uv.fs_stat.result|nil, string|nil, any
-          local stat, err, err_name = vim.uv.fs_stat(filename)
-          if stat ~= nil and stat.type == "file" then
-            -- Open file in current buffer
-            vim.cmd("e " .. filename)
-            vim.cmd(line_number)
-          elseif err ~= nil or err_name ~= nil then
-            print("[GotoLine] ERROR: Failed to stat file `" .. filename .. "`: " .. err_name .. ": " .. err)
+            ---@type uv.fs_stat.result|nil, string|nil, any
+            local stat = vim.uv.fs_stat(filename)
+            if stat ~= nil and stat.type == "file" then
+              vim.cmd("e " .. filename)
+              vim.cmd(line_number)
+
+              matched = true
+              break
+            end
           end
         end
       end
